ca90d06895f59a5933a28d0cbd9a3f2e01484afb
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-05-17 17:02:50 +0300

    add tilt

--- src/Views/First/index.jsx
+++ src/Views/First/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./view.module.scss";
+import Tilt from "react-parallax-tilt";
 
 const First = () => {
   return (
     <div className="view">
-      <div className={styles.view}>first</div>
+      <div className={styles.view}>
+        first
+        <Tilt className={styles.parallax_effect} perspective={500}>
+          <div className={styles.pictureframe}>
+            <div className={styles.test}>Здарова </div>
+            <div>Смотри сюда</div>
+            <div className={styles.blink}>👀</div>
+          </div>
+        </Tilt>
+      </div>
     </div>
   );
 };
