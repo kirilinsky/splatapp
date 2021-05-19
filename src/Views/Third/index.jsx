c981a0eed7bd814405c8bd78df1a5078f98f686a
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-05-19 14:59:22 +0300

    asd

--- src/Views/Third/index.jsx
+++ src/Views/Third/index.jsx
@@ -4,31 +4,31 @@ import styles from "./view.module.scss";
 import before from "./assets/img/before.png";
 import after from "./assets/img/after.png";
 import ReactCompareImage from "react-compare-image";
+import classNames from "classnames";
 
 const Third = () => {
   return (
     <div className="view">
-      <div className="container">
-        <div className={styles.view__wrap}>
-          <h2 className={styles.view__title}>
-            Теперь подобрать отбеливание проще простого
-          </h2>
-          <div className={styles.view_compare}>
-            <ReactCompareImage
-              handle={<Fragment />}
-              hover
-              leftImage={before}
-              rightImage={after}
-            />
+      <div className={styles.withfooter}>
+        <div className={styles.compare}>
+          <div className={classNames("container", styles.view__wrap)}>
+            <h2 className={styles.view__title}>
+              Теперь подобрать отбеливание проще простого
+            </h2>
+            <div className={styles.view_compare}>
+              <ReactCompareImage
+                handle={<Fragment />}
+                hover
+                leftImage={before}
+                rightImage={after}
+              />
+            </div>
+            <button className={classNames("button", styles.button)}>
+              попробовать
+            </button>
           </div>
         </div>
-
-        {/*   <BeforeAfterSlider
-            before={after}
-            after={before}
-            width={640}
-            height={480}
-          /> */}
+        <div className={styles.footer}>footer</div>
       </div>
     </div>
   );
