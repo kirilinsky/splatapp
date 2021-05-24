c2198dd71c15ce2f307d7e006eb2e474516b85d5
AlexandrMonakhov <31701404+AlexandrMonakhov@users.noreply.github.com>
2021-05-24 21:08:28 +0300

    add personal.pdf and little fixes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,6 +4,8 @@ import './Footer.scss'
 
 import logo from '../../assets/img/logo.png'
 
+import personal from '../../assets/personal.pdf'
+
 import vk from './assets/img/vk.svg'
 import inst from './assets/img/inst.svg'
 import fb from './assets/img/fb.svg'
@@ -23,8 +25,7 @@ const Footer = () => {
       </div>
       <div className="footer-bottom">
         <div className="footer-info">
-          <a className="footer-info__link" href="#">Пользовательское соглашение</a>
-          <a className="footer-info__link" href="#">Правила</a>
+          <a className="footer-info__link" href={personal} download target="_blank" rel="noopener noreferrer">Пользовательское соглашение</a>
           <a className="footer-info__link" href="#">Напишите нам</a>
         </div>
         <span className="footer-copy">© SPLAT 2021</span>
